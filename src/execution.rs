07f4a7dffc84a70cd6cde4a102992092a209d7e6
ljedrz <ljedrz@gmail.com>
2017-04-30 17:13:32 +0200

    fix whitespaces, uncomment test

--- src/execution.rs
+++ src/execution.rs
@@ -23,7 +23,7 @@ pub fn run(blc_program: &[u8], blc_argument: &[u8]) -> Result<String, Error> {
     let program = from_binary(blc_program);
     if program.is_err() { return Err(InvalidProgram) }
     let calculation = beta_full(program.unwrap().app(encode(blc_argument))); // safe
-    
+
     Ok(decode(calculation))
 }
 
@@ -38,10 +38,10 @@ mod test {
             11111111111011110010111100111111011111011001011111101111101101000011110011110011100111\
             00111100111100111100001011011000001000000101100000101100000010110000011011000000";
         let s_compressed = [0x1u8, 0x7a, 0x74];
-        
+
         assert_eq!(run(&*inflate, &s_compressed[..]).unwrap(), "000000010111101001110100".to_owned());
     }
-  /*  
+
     #[test]
     fn deflating() {
         // program code from http://www.ioccc.org/2012/tromp/deflate.Blc
@@ -49,9 +49,9 @@ mod test {
             01010111111111111100000000110111000001100010111011001011000001101100001111100001111100\
             00101101110110000000010110011110000010010111111110111111101100010100100000111001110011\
             10100000011100111010000000";
-            
+
         let s_blc = b"00000001011110100111010";
-        
+
         assert_eq!(run(&*deflate, &s_blc[..]).unwrap().as_bytes(), [0x1u8, 0x7a, 0x74]);
-    }*/
+    }
 }
